c02761ed6e6f0a012fa679b97bd6739389d3d782
Yaro <9028029+Cifruktus@users.noreply.github.com>
2022-02-20 13:04:50 +0300

    Text corrections

--- lib/puzzle/cringe.dart
+++ lib/puzzle/cringe.dart
@@ -1,43 +1,43 @@
 // better to use .markdown file instead of this hack
 
 const String intro = """
-Walking along the shore, you've noticed a strange object laying on the ground. 
+While walking along the shore, you noticed a strange object laying on the ground. 
 
-When you got closer it became clear that it's some toy. It seemed like a simple 15-puzzle, but It looked really expensive as if it was made out of gemstones. You decided to take it with you.
+When you got closer it became clear that it's some kind of toy. It seemed like a simple 15-puzzle, but it looked expensive as if it was made out of gemstones. You decided to take it with you.
 
 ![image](resource:assets/img/puzzle.png)
 
-While there is still some time before going back, maybe try to solve it?
+There is still time before you need to start heading back. Maybe we could try to solve it?
 """;
 
 const String firstSolve = """
-For a second everything went black. Feeling of weightless disappeared as quickly as it emerged. After a couple of seconds vision became clear again.
+For a second everything went black. The feeling of confusion and disorientation vanished as quickly as it appeared. After a couple of seconds vision became clear again.
 
-What? What just happened?  
+Wait what? What just happened?
 
-Where am I? This place looks different..
+Where am I? This place looks different.
 
-The puzzle-toy was glowing a little in your hand. Maybe this puzzle is something more than just a toy?  Is it a reason why I'm here? If so, maybe another combination could bring me back? Maybe try to solve it in the **opposite way**?
+The puzzle-toy was glowing a little in your hand. Maybe this puzzle is something more than just a toy? Is it the reason why I am here? If so, maybe another combination could take me back? Let’s try to solve the puzzle in the
+opposite way.
 """;
 
 const String secondSolve = """
-Well, it worked. Not exactly as intended though. Maybe another combination would work better?
+Well, it worked. Not exactly as intended though. There might be another combination that would work better.
 """;
 
 const String solveTheSame = """
-Hmm, now I'm at exact same place as before.. Maybe try to solve the puzzle some other way?
+Hmm, now I'm at exact same place as before... Maybe try to solve the puzzle in another way?
 """;
 
 const String solvePrevious = """
-Seems like I was there before. Well, now we know, that every combination related to a specific place somehow
+Seems like I was here before. Well, now we know, that every combination is related to a specific place somehow.
 """;
 
 const String ending = """
-This place doesn't look exactly familiar but it's definitely Earth!
+This place doesn't look familiar, but I know it is Earth! 
 
 Accidentally puzzle slipped off your hands and fell to the ground braking to the tiniest pieces. You tried to pick up some of the shards, but they all just turned to the finest dust.
 
-Seems all the evidence of this journey just vanished. Who would ever believe in this story? 
 
 Anyway, now there are more important things to do, find the way back home.
 """;
@@ -45,7 +45,7 @@ Anyway, now there are more important things to do, find the way back home.
 const String fromStartToEnd = """
 Something weird just happened, you teleported to another place on Earth.
 
-Is this thing can move you anywhere on the planet? You felt that it wasn't exactly true. This puzzle was capable of much more. 
+Is this puzzle can move you anywhere on the planet? You felt that it wasn't exactly true. This thing was capable of much more. 
 
 You felt a little disappointed that you entered this exact combination. Nothing new happened, story ended here anyway.
 """;
@@ -55,7 +55,7 @@ const String credits = """
 
 This game was made for [Flutter Puzzle Hack](https://flutterhack.devpost.com)
 
-Images source: pixabay
+Images source: [Pixabay](https://pixabay.com)
 
 [Source code](https://github.com/Cifruktus/PuzzleQuest)
 
@@ -65,7 +65,7 @@ Did you find all the places?
 const String about = """
 This game was made for [Flutter Puzzle Hack](https://flutterhack.devpost.com)
 
-Images source: Pixabay
+Images source: [Pixabay](https://pixabay.com)
 
 [Source code](https://github.com/Cifruktus/PuzzleQuest)
 """;
